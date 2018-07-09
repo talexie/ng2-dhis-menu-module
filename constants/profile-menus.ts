9fc7679607a7ca1cdad051a82389b489244827db
rajey <rajeyw@gmail.com>
2018-07-09 20:42:51 +0300

    Fixed profile menu issues

--- constants/profile-menus.ts
+++ constants/profile-menus.ts
@@ -1,28 +1,28 @@
 export const PROFILE_MENUS = [
   {
     name: 'Settings',
-    namespace: '/dhis-web-user-profile',
-    defaultAction: '/dhis-web-user-profile/#/settings',
+    namespace: 'dhis-web-user-profile',
+    defaultAction: 'dhis-web-user-profile/#/settings',
     icon: 'icons/usersettings.png',
     description: ''
   },
   {
     name: 'Profile',
-    namespace: '/dhis-web-user-profile',
-    defaultAction: '/dhis-web-user-profile/#/profile',
+    namespace: 'dhis-web-user-profile',
+    defaultAction: 'dhis-web-user-profile/#/profile',
     icon: 'icons/function-profile.png',
     description: ''
   },
   {
     name: 'Account',
-    namespace: '/dhis-web-user-profile',
-    defaultAction: '/dhis-web-user-profile/#/account',
+    namespace: 'dhis-web-user-profile',
+    defaultAction: 'dhis-web-user-profile/#/account',
     icon: 'icons/function-account.png',
     description: ''
   },
   {
     name: 'Help',
-    namespace: '/dhis-web-commons-about',
+    namespace: 'dhis-web-commons-about',
     defaultAction:
       'https://dhis2.github.io/dhis2-docs/master/en/user/html/dhis2_user_manual_en.html',
     icon: 'icons/function-account.png',
@@ -30,8 +30,8 @@ export const PROFILE_MENUS = [
   },
   {
     name: 'About Dhis2',
-    namespace: '/dhis-web-commons-about',
-    defaultAction: '/dhis-web-commons-about/about.action',
+    namespace: 'dhis-web-commons-about',
+    defaultAction: 'dhis-web-commons-about/about.action',
     icon: 'icons/function-about-dhis2.png',
     description: ''
   }
